55039de06bc71d34e00b8d61c9f4e95504d49f20
Tyler <tyler.manning.dahan53@gmail.com>
2019-05-18 17:28:10 -0400

    Added bash script for automating release cuts

--- save_work.sh
+++ save_work.sh
@@ -1,3 +1,6 @@
+pdflatex cuthesis; pdflatex cuthesis
+bibtex cuthesis; pdflatex cuthesis
 git add -A
 git commit -m "$1"
 git push origin master
+scp cuthesis.pdf ~/Dropbox/Masters\ Research\ -\ Tyler/Thesis/
